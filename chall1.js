7903012ea6d3f766f3a12d0f0b5595860acadde2
mmelamm <145041958+mmelamm@users.noreply.github.com>
2024-05-16 12:36:56 +0100

    Add files via upload

---
+++ chall1.js
@@ -0,0 +1,11 @@
+function openEmail(){
+    let is_checked = document.querySelector("#subscribe").checked
+    console.log(is_checked)
+
+    if(is_checked == true){
+        document.querySelector("#emailDiv").style.display = "block"
+    }
+    else{
+        document.querySelector("#emailDiv").style.display = "none"
+    }}
+    
